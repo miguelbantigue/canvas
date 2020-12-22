55aa4af8504ce236c0bdab4c6fa8d256eabd4f4c
miguelbantigue <72924152+miguelbantigue@users.noreply.github.com>
2020-12-22 10:55:25 +0700

    Add files via upload

---
+++ new1.php
@@ -0,0 +1,298 @@
+<canvas
+ id="Canvas1" width="200" height= "200">
+</canvas>
+
+
+<script>
+      var canvas = document.getElementById('Canvas1');
+      var context = canvas.getContext('2d');
+      
+      context.beginPath();
+      context.rect(55, 30, 70, 110);
+      context.fillStyle = '#000';
+      context.fill();
+      context.lineWidth = 8;
+      context.strokeStyle = '#000';
+      context.stroke();
+
+
+ 
+      createCircle(90,60,10, '#ff0000', 10, '#ff0000');
+      createCircle(90,100,10, '#3cb043', 10, '#3cb043');
+    
+   
+    
+
+  
+
+
+
+	 function createCircle(centerX, centerY, radius, fillcolor, linewidth, strokeColor){
+	  context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = fillcolor;
+      context.fill();
+      context.lineWidth = linewidth;
+      context.strokeStyle = strokeColor;
+      context.stroke();
+
+      
+	  }
+
+  
+
+     
+
+      
+	  
+
+</script>
+
+<canvas
+ id="Canvas2" width="200" height= "200">
+</canvas>
+
+
+<script>
+      var canvas = document.getElementById('Canvas2');
+      var context = canvas.getContext('2d');
+      
+
+     
+      
+      var centerX = canvas.width / 2;
+      var centerY = canvas.height / 2;
+      var radius = 75;
+
+      context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = '#808080';
+      context.fill();
+      context.lineWidth = 8;
+      context.strokeStyle = '#000000';
+      context.stroke();
+
+      
+
+      var centerX = canvas.width / 2;
+      var centerY = canvas.height / 2;
+      var radius = 7;
+
+           
+           
+            createCircle(100,45,4, '#FFFFFF', 1, '##FFA500');
+            createCircle(155,99,4, '#FFFFFF', 1, '##FFA500');
+            createCircle(100,155,4, '#FFFFFF', 1, '##FFA500');
+            createCircle(45,99,4, '#FFFFFF', 1, '##FFA500');
+            createCircle(100,100,4, '#FFFFFF', 1, '##FFA500');
+
+
+context.strokeStyle = '#000000';
+context.beginPath();
+context.moveTo(100, 100);
+context.lineTo(90, 50);
+context.lineWidth = 3;
+context.strokeStyle = '#ffffff';
+context.stroke();
+context.closePath();        
+            
+		
+context.strokeStyle = '#000000';
+context.beginPath();
+context.moveTo(100, 100);
+context.lineTo(50, 60);
+context.lineWidth = 5;
+context.strokeStyle = '#ffffff';
+context.stroke();
+context.closePath();
+
+
+
+
+	 function createCircle(centerX, centerY, radius, fillcolor, linewidth, strokeColor){
+	  context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = fillcolor;
+      context.fill();
+      context.lineWidth = linewidth;
+      context.strokeStyle = strokeColor;
+      context.stroke();
+
+      
+	  }
+
+  
+
+     
+
+      
+	  
+
+</script>
+<canvas
+ id="Canvas3" width="200" height= "200">
+</canvas>
+
+
+<script>
+      var canvas = document.getElementById('Canvas3');
+      var context = canvas.getContext('2d');
+      
+     
+     
+      context.beginPath();
+      context.rect(50, 100, 140, 80);
+      context.fillStyle = '#000000';
+      context.fill();
+      context.lineWidth = 5;
+      context.strokeStyle = '#b3bdb8';
+      context.stroke();
+	  
+	  
+	  
+      createCircle(70,105,4, '#ffffff', 5, '#000000');
+      createCircle(90,105,4, '#ffffff', 5, '#000000');
+      createCircle(110,105,4, '#ffffff', 5, '#000000');
+      createCircle(135,105,4, '#ffffff', 5, '#000000');
+      createCircle(160,105,4, '#ffffff', 5, '#000000');
+
+   
+
+      
+   
+    
+
+  
+
+
+
+	 function createCircle(centerX, centerY, radius, fillcolor, linewidth, strokeColor){
+	context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = fillcolor;
+      context.fill();
+      context.lineWidth = linewidth;
+      context.strokeStyle = strokeColor;
+      context.stroke();
+
+      
+	  }
+
+  
+
+     
+
+      
+	  
+
+</script>
+
+<canvas
+ id="Canvas4" width="200" height= "200">
+</canvas>
+
+
+<script>
+      var canvas = document.getElementById('Canvas4');
+      var context = canvas.getContext('2d');
+      
+     
+
+
+ 
+      createCircle(90,60,10, '#FFD700', 15, '#FFD700');
+      createCircle(90,100,10, '#000000', 10, '#000000');
+	  createCircle(90,130,10, '#000000', 2, '#000000');
+    
+   
+    
+
+  
+
+
+
+	 function createCircle(centerX, centerY, radius, fillcolor, linewidth, strokeColor){
+	  context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = fillcolor;
+      context.fill();
+      context.lineWidth = linewidth;
+      context.strokeStyle = strokeColor;
+      context.stroke();
+
+      
+	  }
+
+  
+
+     
+
+      
+	  
+
+</script>
+<canvas
+ id="Canvas5" width="200" height= "200">
+</canvas>
+
+
+<script>
+      var canvas = document.getElementById('Canvas5');
+      var context = canvas.getContext('2d');
+      
+
+     
+      
+     
+     
+
+      context.beginPath();
+      context.rect(10, 65, 140, 90);
+      context.fillStyle = '#d9dddc';
+      context.fill();
+      context.strokeStyle = '#000';
+      context.stroke();
+	  
+	  context.beginPath();
+      context.rect(170, 90,100, 30);
+      context.fillStyle = '#000';
+      context.fill();
+      context.strokeStyle = '#d9dddc';
+      context.stroke();
+
+      createCircle(40,150,4, '#000000', 40, '#000000');
+      createCircle(140,150,4, '#000000', 40, '#000000');
+
+	  context.beginPath();
+      context.rect(170, 90,100, 30);
+      context.fillStyle = '#000';
+      context.fill();
+      context.strokeStyle = '#d9dddc';
+      context.stroke();
+	  
+	  
+
+
+
+	 function createCircle(centerX, centerY, radius, fillcolor, linewidth, strokeColor){
+	context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = fillcolor;
+      context.fill();
+      context.lineWidth = linewidth;
+      context.strokeStyle = strokeColor;
+      context.stroke();
+
+      
+	  }
+
+  
+
+     
+
+      
+	  
+
+</script>
+
+
